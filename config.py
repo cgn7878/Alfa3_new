5fb41cc46e2f3c27edafd73d6ab7eacf21713bec
cgn7878 <cagancifci@yandex.com>
2025-07-20 11:53:02 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+# Telegram bot ayarları
+TELEGRAM_TOKEN = "7981029346:AAHzDEJtHmZt8V0ZEt7JoT59stlKn1_4DKk"
+TELEGRAM_CHAT_ID = "7831759991"
+
+# API ayarları
+COINGECKO_API_BASE = "https://api.coingecko.com/api/v3"
